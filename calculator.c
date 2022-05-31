007dd278e06aef6c8835ab423d08bd56996036f4
Ismielabir <ismielabir286@gmail.com>
2022-05-31 19:41:43 +0600

    first commit

---
+++ calculator.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main(){
+    double num1, num2, ans;
+    char sign;
+
+    printf("Enter the value of your calculation:\n\n");
+    scanf("%lf %c %lf", &num1, &sign,&num2);
+
+    switch(sign){
+    case '+':
+        ans=num1+num2;
+        break;
+        case '-':
+        ans=num1-num2;
+        break;
+        case '*':
+        ans=num1*num2;
+        break;
+        case '/':
+        ans=num1/num2;
+        break;
+        case '^':
+        ans=pow(num1,num2);
+        break;
+
+
+        default:
+            goto error;
+        break;4.
+    }
+    printf("%.15g%c%.15g = %.5g\n\n", num1, sign, num2, ans);
+    goto exit;
+    error:
+        printf("Please check your input again.\n\n");
+exit:
+
+    return 0;
+
+
+
+}
